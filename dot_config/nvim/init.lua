a4f59467d1af69cea3d6d1088a43283b83468670
Daiki Kajiwara <chick.developer@gmail.com>
2026-02-26 00:00:42 +0900

    feat(nvim): LSP を導入（mason + lua_ls）
    - mason.nvim・nvim-lspconfig・mason-lspconfig.nvim を追加
    - nvim 0.11+ の新 API（vim.lsp.config）で lua_ls を設定
    - lua_ls を自動インストール対象に設定

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -227,6 +227,39 @@ require("lazy").setup({
     end,
   },
 
+  -- LSP
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  { "neovim/nvim-lspconfig" },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      -- nvim 0.11+ の新 API: vim.lsp.config / vim.lsp.enable
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+      })
+    end,
+  },
+
   -- Dashboard (スタートスクリーン)
   {
     "nvimdev/dashboard-nvim",

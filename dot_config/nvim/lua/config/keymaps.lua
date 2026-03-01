0b5fabd5c9bb5a85564ec2723ccb563c68aecddb
Daiki Kajiwara <chick.developer@gmail.com>
2026-03-01 23:40:05 +0900

    feat(nvim): add LSP keymaps for Neovim 0.11

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,10 @@
 -- jj で Normal モードに戻る
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+-- LSP (Neovim 0.11 standard)
+vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "LSP References" })
+vim.keymap.set("n", "grd", vim.lsp.buf.definition, { desc = "LSP Definition" })
+vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "LSP Rename" })
+vim.keymap.set("n", "gra", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { desc = "LSP Implementation" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
